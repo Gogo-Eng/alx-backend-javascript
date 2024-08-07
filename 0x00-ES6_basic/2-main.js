82483aea320e83d7eeb703b85d66f19fe855d155
Gogo-Eng <progressgogochinda@gmail.com>
2024-08-07 08:41:40 +0100

    default

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import getNeighborhoodsList from './2-arrow.js';
+
+// eslint-disable-next-line
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
