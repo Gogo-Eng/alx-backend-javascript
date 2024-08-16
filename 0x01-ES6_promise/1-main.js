3f13c3f574c80f1993fa984256e46c7bfc5db56d
Gogo-Eng <progressgogochinda@gmail.com>
2024-08-16 14:20:24 +0100

    new Error

---
+++ 0x01-ES6_promise/1-main.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line
+import getFullResponseFromAPI from './1-promise';
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  const allStat = {
+    status: 200,
+    body: 'Success',
+  };
+  const info = 'The fake API is not working curently';
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(allStat);
+    } else {
+      reject(new Error(info));
+    }
+  });
+}
