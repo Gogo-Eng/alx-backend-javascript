1937f6a53bab487baf03c7c2d337e20b1415bd58
Gogo-Eng <progressgogochinda@gmail.com>
2024-08-21 17:22:18 +0100

    resolve and reject

--- 0x01-ES6_promise/4-main.js
+++ 0x01-ES6_promise/4-main.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line
 import signUpUser from "./4-user-promise";
 
-console.log(signUpUser('Bob', 'Dylan'));
+signUpUser('Bob', 'Dylan').then((go) => {
+  console.log(go);
+});
---
+++ 0x01-ES6_promise/6-main.js
@@ -0,0 +1,10 @@
+import handleProfileSignup from './6-final-user';
+
+handleProfileSignup("Bob", "Dylan", 'bob_dylan.jpg').then((hh) => {
+  console.log(hh);
+});
+
+/*
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+// output Promise { <pending> }
+*/
