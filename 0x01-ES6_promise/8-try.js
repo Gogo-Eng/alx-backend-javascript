27005370497142d4ab6022653eecd1ad0baf3f55
Gogo-Eng <progressgogochinda@gmail.com>
2024-08-22 14:02:12 +0100

    throw error

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line consistent-return
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  try {
+    const result = numerator / denominator;
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+const show = divideFunction(6, 90);
+console.log(show);
