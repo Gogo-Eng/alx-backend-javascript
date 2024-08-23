bdb07f55bcf0e19c967ee18d9557a5ca473c380b
Gogo-Eng <progressgogochinda@gmail.com>
2024-08-23 15:52:59 +0100

    3rd promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,13 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(res[0].body, res[1].firstName, res[1].lastName);
+    .then(([heart, soul]) => {
+      console.log(heart.body, soul.firstName, soul.lastName);
     })
     .catch(() => console.log('Signup system offline'));
 }
+
+/*
+.then((res) => {
+  console.log(res[0].body, res[1].firstName, res[1].lastName);
+*/
