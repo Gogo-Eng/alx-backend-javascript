49912c6496e3782c10b3a03dc4ae52bb09043ec3
Gogo-Eng <progressgogochinda@gmail.com>
2024-09-02 16:39:17 +0100

    get and set

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,14 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
   }
+
   set name(thename) {
     if (typeof thename !== 'string') {
       throw new TypeError('Name must be a string');
@@ -18,16 +19,18 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
+
   set length(thelength) {
     if (typeof thelength !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = thelength; 
+    this._length = thelength;
   }
 
   get students() {
     return this._students;
   }
+
   set students(allstudents) {
     if (!Array.isArray(allstudents) || !allstudents.every((students) => typeof students === 'string')) {
       throw new TypeError('Students must be an array of strings');
